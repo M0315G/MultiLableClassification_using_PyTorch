f4378f5b2a58b1e868416a6b415eb0e13862a717
M0315 <manish15gupta03@gmail.com>
2021-04-17 12:26:03 +0530

    Added Validation script

--- src/engine.py
+++ src/engine.py
@@ -28,3 +28,26 @@ def train(model, dataloader, optimizer, criterion, train_data, device):
     train_loss = train_running_loss/counter
     
     return train_loss
+
+
+def validate(model, dataloader, criterion, val_data, device):
+    
+    print("Validating..")
+    model.eval()
+    counter = 0
+    val_running_loss = 0.0
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(dataloader), total=int(len(val_data)/dataloader.batch_size)):
+            counter += 1
+            data, target = data['image'].to(device), data['label'].to(device)
+            outputs = model(data)
+            
+            # applu sigmoid activation function
+            outputs = torch.sigmoid(outputs)
+            loss = criterion(outputs, target)
+            val_running_loss += loss.item()
+            
+        val_loss = val_running_loss / counter
+        return val_loss
+    
+    
